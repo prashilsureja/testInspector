420d05af1629ce97bb6fb6c7b57bb5456cafac16
Prashil Sureja <prashil.sureja@clairvoyantsoft.com>
2016-10-12 11:06:02 -0700

    test

--- src/main/java/com/prashil/inspector/PrashilInspector.java
+++ src/main/java/com/prashil/inspector/PrashilInspector.java
@@ -12,7 +12,7 @@ import org.springframework.context.annotation.ComponentScan;
 @SpringBootApplication
 public class PrashilInspector {
 
-   11 public static void main(String[] args) {
+   112 public static void main(String[] args) {
         ApplicationContext ctx = SpringApplication.run(PrashilInspector.class, args);
     }
 
